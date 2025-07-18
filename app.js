ba417d24e50e6918de42731c949a107f80ba8a22
kamiye <kamiye31@gmail.com>
2025-07-18 17:42:07 +0100

    Refactor error handling middleware for improved error management and response structure

--- app.js
+++ app.js
@@ -4,11 +4,18 @@ import userRouter from './routes/user.routes.js';
 import subRouter from './routes/sub.routes.js';
 import authRouter from './routes/auth.routes.js';
 import connectToDatabase from './database/mongodb.js';
+import errorMiddleware from './middlewares/error.middleware.js';
+import cookieParser from 'cookie-parser';
 
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/users', userRouter);
-app.use('/api/v1/subs', subRouter);
+app.use('/api/v1/subs', subRouter); 
+app.use(errorMiddleware);
 
 
 
--- middlewares/error.middleware.js
+++ middlewares/error.middleware.js
@@ -1,9 +1,40 @@
 const errorMiddleware = (err, req, res, next) => {
-  console.error(err.stack);
-  
-  res.status(err.status || 500).json({
-    success: false,
-    message: err.message || 'Internal Server Error',
-    error: process.env.NODE_ENV === 'development' ? err : {}
-  });
+  try {
+  let error = { ...err };
+  error.message = err.message || 'Internal Server Error';
+  console.error(err);
+
+  // mongoose bad objectid error 
+  if (err.name === 'CastError') {
+    const message = `Resource not found. Invalid: ${err.path}`;
+    error = new Error(message);
+    error.status = 404;
+  }
+    // mongoose duplicate key error
+    if (err.code === 11000) {
+      const message = `Duplicate field value entered: ${Object.keys(err.keyValue).join(', ')}`;
+      error = new Error(message);
+      error.status = 400;
+    }
+
+    // mongoose validation error
+    if (err.name === 'ValidationError') {
+      const message = Object.values(err.errors).map(val => val.message).join(', ');
+      error = new Error(message);
+      error.status = 400;
+    }
+
+    res.status(error.status || 500).json({
+      success: false,
+        message: error.message || 'Internal Server Error',
+    });
+
+
+
+} catch (error) {
+  next(error);
 }
+};
+export default errorMiddleware;
+
+
